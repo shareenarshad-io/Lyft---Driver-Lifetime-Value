709aa1a282e07bbac0b03698ceadf3e8257bd24c
Shareen Arshad <107083486+shareenarshad-io@users.noreply.github.com>
2025-05-11 03:39:47 -0400

    Imported libraries

---
+++ lyft.py
@@ -0,0 +1,17 @@
+'''
+We would like to answer some questions after exploring and analyzing the data, questions are :
+'''
+
+# Import the required libraries
+import pandas as pd # for data manipulation and analysis
+import numpy as np # for numerical operations
+import matplotlib.pyplot as plt # for data visualization
+import seaborn as sns # for advanced data visualization
+
+#Recommend a Driver's Lifetime Value (i.e., the value of a driver to Lyft over the entire projected lifetime of a driver).
+
+
+#What are the main factors that affect a driver's lifetime value?
+#What is the average projected lifetime of a driver? That is, once a driver is onboarded, how long do they typically continue driving with Lyft?
+#Do all drivers act alike? Are there specific segments of drivers that generate more value for Lyft than the average driver?
+#What actionable recommendations are there for the business?
